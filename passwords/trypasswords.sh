b3d1e5ebb8772b7d74ee734f88323466e3b8f0b7
Marco Delmastro <marco.delmastro@gmail.com>
2021-11-12 12:26:31 +0100

    Trying to hack the user passwords

--- passwords/trypasswords.sh
+++ passwords/trypasswords.sh
@@ -1,11 +1,2 @@
 #!/bin/zsh
-for user in ftd knr gardener ohmega yang howie hmonk bbarker
-do
-    echo Hacking user $user
-    rm -f trypasswords_$user.ins
-    cp -f trypasswords.ins trypasswords_$user.ins
-    echo ./hackfix.exe $user >> trypasswords_$user.ins
-    echo exit >> trypasswords_$user.ins
-    cat trypasswords_$user.ins | pypy3 ../um.py ../umz/dump.um - | tee pass_${user}.log
-    rm -f trypasswords_$user.ins
-done
+cat trypasswords.ins | pypy3 ../um.py ../umz/dump.um - |& tee trypasswords.log
