1ed74431489401f3eefb031f649d18d406c08d03
Marco Delmastro <marco.delmastro@gmail.com>
2021-11-17 17:42:23 +0100

    Password hacking with c++ UM

--- passwords/trypasswords.sh
+++ passwords/trypasswords.sh
@@ -1,2 +1,4 @@
 #!/bin/zsh
-cat trypasswords.ins | pypy3 ../um.py ../umz/dump.um - |& tee trypasswords.log
+
+#cat trypasswords.ins | pypy3 ../um.py ../umz/dump.um - | tee trypasswords.log
+cat trypasswords.ins | ../um ../umz/dump.um
